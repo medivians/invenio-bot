3475b455857af65c2cc04e0fcf1723260a63661c
Leonardo Ramos Duarte <leonardo.duarte@contaquanto.com.br>
2023-03-24 14:17:10 -0300

    chore: clean commented code

--- scraper/wiki/wiki.go
+++ scraper/wiki/wiki.go
@@ -86,23 +86,6 @@ func (w *Wiki) fetchItem(n string) (*http.Response, error) {
 	if err != nil {
 		return nil, err
 	}
-
-	// req.Header.Add("authority", "wiki.mediviastats.info")
-	// req.Header.Add("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
-	// req.Header.Add("accept-language", "en-US,en;q=0.9,pt-BR;q=0.8,pt;q=0.7")
-	// req.Header.Add("cache-control", "max-age=0")
-	// req.Header.Add("dnt", "1")
-	// req.Header.Add("if-modified-since", "Sun, 18 Dec 2022 01:54:44 GMT")
-	// req.Header.Add("referer", "https://www.google.com/")
-	// req.Header.Add("sec-ch-ua", "Google Chrome;v=111, Not(A:Brand;v=8, Chromium;v=111")
-	// req.Header.Add("sec-ch-ua-mobile", "?0")
-	// req.Header.Add("sec-ch-ua-platform", "Windows")
-	// req.Header.Add("sec-fetch-dest", "document")
-	// req.Header.Add("sec-fetch-mode", "navigate")
-	// req.Header.Add("sec-fetch-site", "cross-site")
-	// req.Header.Add("sec-fetch-user", "?1")
-	// req.Header.Add("upgrade-insecure-requests", "1")
-	// req.Header.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36")
 	return w.httpCli.Do(req)
 }
 
